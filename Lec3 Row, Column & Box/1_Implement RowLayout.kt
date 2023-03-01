290e62c53faad38c832a2a64f2961ca0ecc39cd1
Utkarsh Saxena <utkarshsaxena900@gmail.com>
2023-03-01 18:35:06 +0530

    Create 1_Implement RowLayout.kt

---
+++ Lec3 Row, Column & Box/1_Implement RowLayout.kt
@@ -0,0 +1,36 @@
+// Output for this code is attached in Pic 1 in main Readme.
+
+package com.example.helloworld.utkarsh
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+
+@Composable
+fun Three() {
+    RowLayout()
+}
+
+@Composable
+fun RowLayout() {
+    //row aligns data horizontally
+    Row(horizontalArrangement = Arrangement.SpaceEvenly,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.run {
+            background(Color.Yellow)
+        }
+    )
+    {
+        Text(text = "Apple")
+        Text(text = "Banana")
+        Text(text = "Oranges")
+    }
+
+}
